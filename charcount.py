8ab8de6f37adbedcc6699b2fbe88efb26d98f720
Gabriel Lee <gabriel.lee@central1.com>
2020-01-27 16:25:46 -0800

    Add comments and update message to stdout

--- charcount.py
+++ charcount.py
@@ -6,33 +6,35 @@ parser.add_argument('-i', '--input', help='name of the input text file', require
 parser.add_argument('-o', '--output', help='name of the output CSV file')
 args = parser.parse_args()
 
-# Read input file
+# Read input text file
 in_f = open(args.input, "r")
 
-# Create output file
+# Create output CSV file
 if args.output is not None:
     out_f = open(args.output, "w")
 else:
     out_f = open("charcount.csv", "w")
-out_f.write("letter,count,probability\n")
 
+out_f.write("letter,count,probability\n")
 contents = in_f.read().lower()
+
+# Set global variables
 eng_alpha = "abcdefghijklmnopqrstuvwxyz"
 char_dict = {}
 char_count = 0
+rel_prob_sum = 0
 
+# Get each character count and calculate max char_count
 for chr in eng_alpha:
     char_dict[chr] = contents.count(chr)
     char_count += char_dict[chr]
 
-rel_prob_sum = 0
-
+# Calculate relative probability and output data to CSV
 for chr in eng_alpha:
     rel_prob = float(char_dict[chr])/float(char_count)
     out_f.write("{},{},{}\n".format(chr,char_dict[chr], rel_prob))
     rel_prob_sum += rel_prob
 
-print(rel_prob_sum)
-
-# print(char_dict)
-# print(char_count)
+print("Relative Probability: {}".format(rel_prob_sum))
+print("Dictionary: {}".format(char_dict))
+print("Character Count: {}".format(char_count))

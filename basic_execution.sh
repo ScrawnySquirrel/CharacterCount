1aee487e7dde0d05b929162b292b3f6a0485db73
Gabriel Lee <gabriel.lee@central1.com>
2020-01-30 15:03:33 -0800

    Update encrypt value

--- basic_execution.sh
+++ basic_execution.sh
@@ -6,7 +6,7 @@ if [[ -z $1 ]]; then
 
   # Generated ciphertext
   ./caesar.out textfiles/aohf.txt textfiles/aohf_ct3.txt e 3
-  ./caesar.out textfiles/pap.txt textfiles/pap_ct7.txt e 7
+  ./caesar.out textfiles/pap.txt textfiles/pap_ct5.txt e 5
 
   # Get character count and distribution
   mkdir csvfiles
@@ -17,7 +17,7 @@ if [[ -z $1 ]]; then
   for j in {e,t,a,o,i,n}; do
     echo "Letter: $j" >> csvfiles/aohf_condprob.csv
     python condprob.py -p csvfiles/aohf.csv -c csvfiles/aohf_ct3.csv -m $j -o csvfiles/aohf_condprob.csv
-    python condprob.py -p csvfiles/pap.csv -c csvfiles/pap_ct7.csv -m $j -o csvfiles/pap_condprob.csv
+    python condprob.py -p csvfiles/pap.csv -c csvfiles/pap_ct5.csv -m $j -o csvfiles/pap_condprob.csv
   done
 elif [[ $1 == "-c" ]]; then
   # Clean up

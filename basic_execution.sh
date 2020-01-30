f836a20ffe38a695b8c19c07f12721de750741bb
Gabriel Lee <gabriel.lee@central1.com>
2020-01-30 13:45:04 -0800

    Add condprob.py execution

--- basic_execution.sh
+++ basic_execution.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 if [[ -z $1 ]]; then
+  # Create caesar.c executable
   gcc caesar.c -o caesar.out -w
 
+  # Generated ciphertext
   ./caesar.out textfiles/aohf.txt textfiles/aohf_ct3.txt e 3
   ./caesar.out textfiles/pap.txt textfiles/pap_ct7.txt e 7
 
+  # Get character count and distribution
+  mkdir csvfiles
   for i in $(ls textfiles); do
     python charcount.py -i textfiles/$i -o csvfiles/"$(basename "$i" .txt).csv"
+  done
+
+  for j in {e,t,a,o,i,n}; do
+    python condprob.py -p csvfiles/aohf.csv -c csvfiles/aohf_ct3.csv -m $j -o csvfiles/aohf_condprob.csv
+    python condprob.py -p csvfiles/pap.csv -c csvfiles/pap_ct7.csv -m $j -o csvfiles/pap_condprob.csv
   done
 elif [[ $1 == "-c" ]]; then
   # Clean up
